8dcd0a5bf5288ef82cc53d0afae67a5a95b24ace
catea792 <79623874+catea792@users.noreply.github.com>
2021-02-25 20:00:08 +0700

    Update Test_SOFDE.java

--- System-Of-First-Degree-Equations/Test_SOFDE.java
+++ System-Of-First-Degree-Equations/Test_SOFDE.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Test_SOFDE {
     public static void main(String[] args){
     double a11, a12, b1, a21, a22, b2;
-        System.out.println("Enter the values for the program: ");
+        System.out.println("NHap tham so he phuong trinh: ");
         System.out.print("a11 = ");
     a11 = new Scanner(System.in).nextDouble();
         System.out.print("a12 = ");
